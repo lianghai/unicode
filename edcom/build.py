c8b4a302b6418ee0c3eb87e9b0f997b78b560be7
Liang Hai <lianghai@gmail.com>
2022-02-06 03:20:56 +0100

    Basic transformation

--- edcom/build.py
+++ edcom/build.py
@@ -1,11 +1,19 @@
 from copy import deepcopy
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Literal
 
 import html5lib
 from lxml.etree import _Element as Element
 
 TREE_TYPE = "lxml"
+
+parser = html5lib.HTMLParser(
+    tree=html5lib.treebuilders.getTreeBuilder(TREE_TYPE),
+    namespaceHTMLElements=False,
+)
+walker = html5lib.getTreeWalker(TREE_TYPE)
+serializer = html5lib.serializer.HTMLSerializer()
 
 repo = Path(__file__).parent
 
@@ -19,42 +27,18 @@ class Source:
     def __init__(self, directory: Path):
         self.directory = directory
         self.path_to_html = {
-            i.relative_to(self.directory): html5lib.parse(
-                i.read_bytes(), treebuilder=TREE_TYPE, namespaceHTMLElements=False
-            )
+            i.relative_to(self.directory): parser.parse(i.read_bytes())
             for i in self.directory.rglob("*.html")
         }
 
-    def transform_element(self, element: Element, /, parent: Element):
-
-        print(element.tag)
-        match element.tag:
-            case "char":
-                parent.replace(element, element.makeelement("x", {}, None))
-            case _:
-                if (attribute := "placeholder") in element.keys() and element.text:
-                    transformed = element.text.replace("X.X", "1.1").replace("X-X", "1-9")  # TODO
-                    element.text = transformed  # type: ignore
-                    del element.attrib[attribute]
-
     def build(self, directory: Path):
-
-        walker = html5lib.getTreeWalker(TREE_TYPE)
-        serializer = html5lib.serializer.HTMLSerializer()
 
         for relative_path, source in self.path_to_html.items():
 
-            if relative_path.stem not in {
-                "Unicode Design Principles",
-                # "Compatibility Characters",
-                # "Han",
-            }:
-                continue
-
             transformed = deepcopy(source)
-            for parent in transformed.iter():  # type: ignore
-                for element in parent:
-                    self.transform_element(element, parent)
+
+            for element in transformed.iter(tag=None):
+                self.transform_element(element)
 
             serialized = "".join(serializer.serialize(walker(transformed)) or [])
 
@@ -62,8 +46,47 @@ class Source:
             path.parent.mkdir(parents=True, exist_ok=True)
             path.write_text(serialized)
 
+    def transform_element(self, element: Element, /):
+
+        match element.tag:
+
+            case "char":
+
+                transformed = element.makeelement("span", {"class": "char"}, None)
+                transformed.text, transformed.tail = element.text, element.tail
+
+                element.getparent().replace(element, transformed)
+
+            case "h1" | "figcaption" if (mode := "numbering") in element.keys():
+
+                self.expand_placeholder_text_in_element(element, mode)
+
+            case "a" if (mode := "referencing") in element.keys():
+
+                self.expand_placeholder_text_in_element(element, mode)
+
+                wrap = element.makeelement("cite", {}, None)
+                wrap.tail, element.tail = element.tail, None  # type: ignore
+
+                element.getparent().replace(element, wrap)
+                wrap.append(element)
+
+    PLACEHOLDER_MODE = Literal["numbering", "referencing"]
+
+    def expand_placeholder_text_in_element(
+        self, element: Element, /, mode: PLACEHOLDER_MODE = None
+    ):
+
+        text: str = element.text
+
+        if "X.X" in text:
+            text = text.replace("X.X", "1.1")  # TODO
+        elif "X-X" in text:
+            text = text.replace("X-X", "1-9")  # TODO
+
+        element.text = text  # type: ignore
+        del element.attrib[mode]
+
 
 if __name__ == "__main__":
-
-    source = Source(repo / "source")
-    source.build(repo / "distribution")
+    Source(repo / "source").build(repo / "distribution")
